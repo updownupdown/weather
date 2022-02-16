8cfad4e0e827629162294fec2102257b70faba16
updownupdown <updownupdown@gmail.com>
2022-02-15 20:06:31 -0500

    Remove comments in backend

--- backend/index.js
+++ backend/index.js
@@ -33,32 +33,6 @@ app.get("/lat/:lat/lon/:lon", (req, res) => {
     });
 });
 
-// function geocode() {
-
-//   fetch(geocodingAPIUrl)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setLatLon({ lat: data[0].lat, lon: data[0].lon });
-//     });
-// }
-
-// useEffect(() => {
-//   geocode();
-// }, [location]);
-
-// function fetchWeatherData() {
-//   if (latLon.lat === 0 && latLon.lon === 0) return;
-
-//   const oneCallAPIUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&units=metric&exclude=minutely&appid=${apiKey}`;
-
-//   fetch(oneCallAPIUrl)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setWeatherData(data);
-//       console.log(data);
-//     });
-// }
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
